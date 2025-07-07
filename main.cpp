e25e9becacfe40fc121a58f25e125403abc95d0a
Anna Prokopchenko <aprokopchenko@kse.org.ua>
2025-07-07 13:01:08 +0300

    fixes, now works with unary minus

--- main.cpp
+++ main.cpp
@@ -10,130 +10,16 @@
 
 //how to control user do using whitespace between something correct 
 //like inta and int a
-//how to unary minus
-//how to control i fuser enters something in recognized tokens
 
 
 namespace parsing {
 
-    const std::string recognizedOperators = "-+/*()";
-    const std::string recognizedSymbols = recognizedOperators + "abcdefghijklmnopqrstuvwxyz" + " " + "1234567890" + ",";
-
-    std::list < std::string > Parse(std::string input) {
-
-        char cursymbol;
-        bool iscursymbolblankspaceOrcomma = false;
-        std::string curNum;
-        std::string funcOrVar;
-        std::string operand;
-        std::list < std::string > tokens;
-
-        for (size_t i = 0; i < input.size(); ++i) {
-            iscursymbolblankspaceOrcomma = false;
-
-            cursymbol = input[i];
-
-            if (isdigit(cursymbol)) {
-                curNum.push_back(cursymbol);
-            }
-            else if (isalpha(cursymbol)) {//what if variable name has number in it
-                funcOrVar.push_back(cursymbol);
-            }
-            else if (recognizedOperators.contains(cursymbol)) {
-                operand.push_back(cursymbol);
-            }
-            else if (cursymbol == ' ' || cursymbol == ',') {
-                iscursymbolblankspaceOrcomma = true;
-            }
-            else {
-                std::cout << "unrecognized input: " << cursymbol;
-                exit(-1);
-            }
-
-            if (iscursymbolblankspaceOrcomma) {
-                if ((curNum.length() > 0) + (funcOrVar.length() > 0) + (operand.length() > 0) > 1) {
-                    std::cout << "incorrect input";
-                    exit(-1);
-                }
-
-                if (curNum.length() > 0) {
-                    tokens.push_back(curNum);
-                    curNum.clear();
-                }
-                else if (funcOrVar.length() > 0) {
-                    tokens.push_back(funcOrVar);
-                    funcOrVar.clear();
-                }
-                else if (operand.length() > 0) {
-                    tokens.push_back(operand);
-                    operand.clear();
-                }
-
-            }
-            else {
-                if (isdigit(cursymbol)) {
-                    if (funcOrVar.length() > 0 && operand.length() > 0) {
-                        std::cout << "incorrect input";
-                        exit(-1);
-                    }
-
-                    if (funcOrVar.length() > 0) {
-                        tokens.push_back(funcOrVar);
-                        funcOrVar.clear();
-                    }
-                    else if (operand.length() > 0) {
-                        tokens.push_back(operand);
-                        operand.clear();
-                    }
-
-                }
-                else if (isalpha(cursymbol)) {
-                    if (curNum.length() > 0 && operand.length() > 0) {
-                        std::cout << "incorrect input";
-                        exit(-1);
-                    }
-
-                    if (curNum.length() > 0) {
-                        tokens.push_back(curNum);
-                        curNum.clear();
-                    }
-                    else if (operand.length() > 0) {
-                        tokens.push_back(operand);
-                        operand.clear();
-                    }
-                }
-                else if (recognizedOperators.contains(cursymbol)) {
-                    if (curNum.length() > 0 && funcOrVar.length() > 0) {
-                        std::cout << "incorrect input";
-                        exit(-1);
-                    }
-
-                    if (curNum.length() > 0) {
-                        tokens.push_back(curNum);
-                        curNum.clear();
-                    }
-                    else if (funcOrVar.length() > 0) {
-                        tokens.push_back(funcOrVar);
-                        funcOrVar.clear();
-                    }
-                }
-            }
-        }
-
-        if (curNum.length() > 0) {
-            tokens.push_back(curNum);
-        }
-        else if (funcOrVar.length() > 0) {
-            tokens.push_back(funcOrVar);
-        }
-        else if (operand.length() > 0) {
-            tokens.push_back(operand);
-        }
-
-        return tokens;
-    }
-
     bool IsStringNumber(std::string str) {
+
+        if (str.size() == 0) {
+            return false;
+        }
+
         for (size_t i = 0; i < str.size(); ++i) {
             if (!isdigit(str[i])) {
                 return false;
@@ -142,13 +28,143 @@ namespace parsing {
         return true;
     }
 
+    const std::string recognizedOperators = "-+/*()";
+    const std::string recognizedSymbols = recognizedOperators + "abcdefghijklmnopqrstuvwxyz" + " " + "1234567890" + ",";
+    const std::string recognized_non_number_tokens[12] = {"min","max","abs","pow","(",")","-","+","/","*",",","Negative"};
+    const int recognized_non_number_tokens_count = 12;
+
+    bool isprelastnonblanksymbolacommaordoesntexist(int curindex, std::string input) {
+        for (int i = curindex - 1; i >= 0; --i) {
+
+            if (input[i] != ',' && input[i] != ' ') {
+                return false;
+            }
+
+            if (input[i] == ',') {
+                return true;
+            }
+
+        }
+        return true;
+    }
+
+
+    bool isprelastnonblanksymbolaopeningbracket(int curindex, std::string input) {
+        for (int i = curindex - 1; i >= 0; --i) {
+
+            if (input[i] != '(' && input[i] != ' ') {
+                return false;
+            }
+
+            if (input[i] == '(') {
+                return true;
+            }
+
+        }
+        return false;
+    }
+    bool is_string_in_recognized_non_number_tokens(std::string token) {
+
+        for (int i = 0; i < recognized_non_number_tokens_count;++i) {
+            if (token == recognized_non_number_tokens[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    std::list < std::string > BasicParsing(std::string input) {//split into numbers functions and operators ignore blankspace and comma
+        
+        char cursymbol;
+        std::string curtoken;
+        std::list < std::string > tokens;
+        for (size_t i = 0; i < input.size(); ++i) {
+            cursymbol = input[i];
+
+            if (cursymbol == ' ' || cursymbol == ',') {
+                if (curtoken.size() > 0) {
+                    tokens.push_back(curtoken);
+                    curtoken.clear();
+                }
+
+                if (cursymbol == ',') {
+                    tokens.push_back(",");
+                }
+                continue;
+            }
+
+            if (isdigit(cursymbol)) {
+                curtoken.push_back(cursymbol);
+            }
+            else {
+                if (IsStringNumber(curtoken)) {
+                    tokens.push_back(curtoken);
+                    curtoken.clear();
+                }
+                curtoken.push_back(cursymbol);
+
+                if (is_string_in_recognized_non_number_tokens(curtoken)) {
+                    tokens.push_back(curtoken);
+                    curtoken.clear();
+                }
+            }
+        }
+
+        if (curtoken.size() > 0) {
+            tokens.push_back(curtoken);
+            curtoken.clear();
+        }
+        return tokens;
+    }
+
+    std::list < std::string > detect_and_change_all_unary_minuses(std::list < std::string > tokens) {
+
+        for (std::list < std::string >::iterator i = tokens.begin(), previous = tokens.end(); i != tokens.end(); previous = i, ++i) {
+            std::string curtoken = *i;
+
+            if (curtoken != "-") {
+                continue;
+            }
+
+            if (i == tokens.begin()) {
+                *i = "Negative";
+                continue;
+            }
+            
+            std::string prevvalue = *previous;
+            if (prevvalue == "," || prevvalue == "(") {
+                *i = "Negative";
+            }
+            
+        }
+        return tokens;
+    }
+
+    std::list < std::string > delete_commas(std::list < std::string > tokens) {
+        tokens.remove(",");
+        return tokens;
+    }
+
+
+    bool check_if_all_tokens_are_recognized(std::list < std::string > tokens) {
+        for (std::list < std::string >::iterator i = tokens.begin(); i != tokens.end(); ++i) {
+            std::string curtoken = *i;
+            if (IsStringNumber(curtoken) || is_string_in_recognized_non_number_tokens(curtoken)) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    
 
     bool is_operand1_higher_or_same_precedence_as_operand2(std::string operand1, std::string operand2) {
 
         //operations priority
         //+- 
         //*/
-        //^
         //functions
         int operand1Priority = -1;
         int operand2Priority = -1;
@@ -156,7 +172,7 @@ namespace parsing {
         std::string precedenceByIndex[3];
         precedenceByIndex[0] = "+-";
         precedenceByIndex[1] = "*/";
-        precedenceByIndex[2] = "minmax";
+        precedenceByIndex[2] = "minmaxpowabsNegative";
 
         for (int i = 0;i < 3;++i) {
             if (precedenceByIndex[i].contains(operand1)) {
@@ -257,12 +273,28 @@ namespace parsing {
         else if (operation == "/") {
             return leftnum / rightnum;
         }
+        else if (operation == "pow") {
+            return std::pow(leftnum, rightnum);
+        }
         else {
             std::cout << "operation not found";
             exit(-1);
         }
 
     };
+
+    int executeUnaryOperation(std::string operation, int num) {
+        if (operation == "abs") {
+            return std::abs(num);
+        }
+        else if (operation == "Negative") {
+            return -num;
+        }
+        else {
+            std::cout << "operation not found";
+            exit(-1);
+        }
+    }
 
     int Calculate(std::queue<std::string> expressionInRpn, std::unordered_map<std::string, int> context) { //ReversePolishNotation = rpn
 
@@ -281,14 +313,25 @@ namespace parsing {
                 numbersStack.push(context[curtoken]);
             }
             else {
-                //unary or binary operation? i think i dont have unary operations for know
-                int rightnum = numbersStack.top();
-                numbersStack.pop();
-                int leftnum = numbersStack.top();
-                numbersStack.pop();
+                if (curtoken == "abs" || curtoken == "Negative") {
+                   
+                    int num = numbersStack.top();
+                    numbersStack.pop();
+                    int result = executeUnaryOperation(curtoken, num);
+                    numbersStack.push(result);
 
-                int result = executeBinaryOperation(curtoken, leftnum, rightnum);
-                numbersStack.push(result);
+                }
+                else {
+                    
+                    int rightnum = numbersStack.top();
+                    numbersStack.pop();
+
+                    int leftnum = numbersStack.top();
+                    numbersStack.pop();
+
+                    int result = executeBinaryOperation(curtoken, leftnum, rightnum);
+                    numbersStack.push(result);
+                }
 
             }
 
@@ -309,7 +352,21 @@ int main()
     std::string input;
     std::getline(std::cin, input);
 
-    std::list < std::string > output = parsing::Parse(input);
+    std::list < std::string > output = parsing::BasicParsing(input);
+
+    for (std::list < std::string >::iterator i = output.begin(); i != output.end(); ++i) {
+        std::cout << " " << *i;
+    }
+    std::cout << "\n";
+
+    output = parsing::detect_and_change_all_unary_minuses(output);
+
+    for (std::list < std::string >::iterator i = output.begin(); i != output.end(); ++i) {
+        std::cout << " " << *i;
+    }
+    std::cout << "\n";
+
+    output = parsing::delete_commas(output);
 
     for (std::list < std::string >::iterator i = output.begin(); i != output.end(); ++i) {
         std::cout << " " << *i;
@@ -319,25 +376,18 @@ int main()
     std::unordered_map<std::string, int> context;
 
     std::queue<std::string> q = parsing::ConvertToReversePolishNotation(output);
+    
+    std::queue<std::string> newq;
+    while (q.size() > 0) {
+        std::cout << q.front() << " ";
+        newq.push(q.front());
+        q.pop();
+    }
 
     std::cout << "\n";
 
-    std::cout << parsing::Calculate(q, context);
+    std::cout << parsing::Calculate(newq, context);
 
     return 0;
 }
 
-
-//python script is for testing
-//for now I should create the logic
-//first milestone
-//Basic arithmetic operations: +, -, *, /
-//6 points:
-//> 5 + 3 * 2
-//> 11 #  < ----here and below it is an answer
-
-//reverse polish notation 
-//get input
-//tokenize
-//convert to reverse polish notation
-//calculate
